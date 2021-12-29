5ca8e4ac0a5357b352080a8d5f9b2bbfd4896537
tomoya sunda <sunda0707@icloud.com>
2021-12-29 15:47:27 +0900

    SuppliersController を実装する

--- rails/app/controllers/api/v1/suppliers_controller.rb
+++ rails/app/controllers/api/v1/suppliers_controller.rb
@@ -1,16 +1,41 @@
 class Api::V1::SuppliersController < ApplicationController
   def index
+    @suppliers = Supplier.kept
+    render json: { suppliers: SupplierBlueprint.render(@suppliers) }
   end
 
   def create
+    @supplier = Supplier.new(supplier_attributes)
+    return render json: { messages: @supplier.errors.full_messages }, status: :unprocessable_entity if @supplier.invalid?
+
+    @supplier.save!
+    render json: { supplier: SupplierBlueprint.render(@supplier) }
   end
 
   def show
+    @supplier = Supplier.kept.find(params[:id])
+    render json: { supplier: SupplierBlueprint.render(@supplier) }
   end
 
   def update
+    @supplier = Supplier.kept.find(params[:id])
+    @supplier.attributes = supplier_attributes
+    return render json: { messages: @supplier.errors.full_messages }, status: :unprocessable_entity if @supplier.invalid?
+
+    @supplier.save!
+    render json: { supplier: SupplierBlueprint.render(@supplier) }
   end
 
   def destroy
+    @supplier = Supplier.kept.find(params[:id])
+    @supplier.discard!
+
+    head :ok
+  end
+
+  private
+
+  def supplier_attributes
+    params.require(:supplier).permit(:name)
   end
 end
