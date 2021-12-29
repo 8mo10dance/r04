2b2909edd6e7f75c95bf9cc0dc8ada90e1981a70
tomoya sunda <sunda0707@icloud.com>
2021-12-29 12:39:19 +0900

    Supplier モデルを修正する

--- rails/app/models/supplier.rb
+++ rails/app/models/supplier.rb
@@ -1,5 +1,5 @@
 class Supplier < ApplicationRecord
   has_many :products, dependent: :destroy
 
-  validates :name, presence :true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 end
