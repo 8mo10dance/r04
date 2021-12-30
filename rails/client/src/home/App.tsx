672ce9e68b5e01c7c4df966887759755003fb1a2
tomoya sunda <sunda0707@icloud.com>
2021-12-30 20:41:47 +0900

    登録ダイアログの枠だけ作る

--- rails/client/src/home/App.tsx
+++ rails/client/src/home/App.tsx
@@ -1,19 +1,35 @@
 import React from "react";
-import { CssBaseline, Button, Box, Paper } from "@mui/material";
+import {
+  CssBaseline,
+  Button,
+  Box,
+  Paper,
+  Dialog,
+  DialogTitle,
+} from "@mui/material";
 
 import SupplierList from "~/modules/SupplierList";
 
 const App: React.VFC<{}> = () => {
+  const [isOpening, setIsOpening] = React.useState(false);
+
   return (
     <>
       <CssBaseline />
       <Box p={2}>
-        <Button variant="contained" color="primary">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => setIsOpening(true)}
+        >
           登録
         </Button>
         <Paper>
           <SupplierList />
         </Paper>
+        <Dialog open={isOpening} onClose={() => setIsOpening(false)}>
+          <DialogTitle>発注先登録</DialogTitle>
+        </Dialog>
       </Box>
     </>
   );
