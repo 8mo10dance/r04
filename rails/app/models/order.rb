ed820f2d5b586a8d2840bd67842a9af43ab510dc
tomoya sunda <sunda0707@icloud.com>
2022-01-07 00:29:29 +0900

    orders.name に suppliers.name を登録する

--- rails/app/models/order.rb
+++ rails/app/models/order.rb
@@ -2,4 +2,14 @@ class Order < ApplicationRecord
   belongs_to :supplier
 
   validates :name, presence: true
+
+  before_validation :set_name
+
+  private
+
+  def set_name
+    return if supplier.blank?
+
+    self.name = supplier.name
+  end
 end
--- rails/spec/models/order_spec.rb
+++ rails/spec/models/order_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:order) { build :order, supplier: supplier }
+  let(:supplier) { create :supplier }
+
+  describe 'save!' do
+    subject(:save!) { order.save! }
+
+    it 'orders.name に suppliers.name が登録される' do
+      save!
+
+      expect(order.reload.name).to eq supplier.name
+    end
+  end
 end
