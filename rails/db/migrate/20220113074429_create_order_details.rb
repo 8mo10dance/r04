65ec4c64a5803836181e5170e770f20d6c28019b
tomoya sunda <sunda0707@icloud.com>
2022-01-13 16:48:03 +0900

    order_details テーブルにカラムを追加する

--- rails/db/migrate/20220113074429_create_order_details.rb
+++ rails/db/migrate/20220113074429_create_order_details.rb
@@ -1,6 +1,10 @@
 class CreateOrderDetails < ActiveRecord::Migration[7.0]
   def change
     create_table :order_details do |t|
+      t.string :name, null: false, default: ''
+      t.integer :quantity, null: false, default: 0
+      t.references :product, foreign_key: true
+      t.references :order, foreign_key: true
 
       t.timestamps
     end
--- rails/spec/factories/order_details.rb
+++ rails/spec/factories/order_details.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :order_detail do
-    
+    association :order
+    association :product
+
+    name { Faker::Lorem.word }
+    quantity { rand(1000) }
   end
 end
